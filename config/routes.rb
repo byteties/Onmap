e191e12f568f02abc6aacde81aaedf390085dd27
Chayangkoon <chayangkoondokhom@gmail.com>
2017-02-06 13:36:44 +0700

    NSC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root 'infomaps#test'
+	root 'infomaps#index'
 
 	get 'infomaps/index' ,to: 'infomaps#index' , as: 'infomaps_index'
 	post 'infomaps/circle' ,to: 'infomaps#circle' ,as: 'infomaps_circle'
