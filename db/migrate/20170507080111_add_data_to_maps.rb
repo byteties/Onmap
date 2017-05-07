09ef6f96cad4ebe8a89285877ab0a4a4d94d6266
Chayangkoon <chayangkoondokhom@gmail.com>
2017-05-07 15:01:59 +0700

    change order database

--- db/migrate/20170507080111_add_data_to_maps.rb
+++ db/migrate/20170507080111_add_data_to_maps.rb
@@ -1,14 +1,14 @@
 class AddDataToMaps < ActiveRecord::Migration[5.0]
   def change
   	create_table :infomaps do |t|
-  		t.string :name_station
-  		t.string :name_location
+  	  t.string :name_station
+  	  t.string :name_location
       t.string :river
       t.string :sub_district
       t.string :district
       t.st_point :lonlat, geographic: true
       t.st_polygon :area, geographic: true
-  		t.timestamps null: false
+  	  t.timestamps null: false
   	end
   end
 end
